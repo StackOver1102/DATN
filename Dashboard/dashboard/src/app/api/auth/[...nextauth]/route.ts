d170c9c381ba1bc10945bba1440959eb123493f7
Sotatek-TuNguyen6 <tu.nguyen6@sotatek.com>
2025-08-13 16:08:30 +0700

    update

--- Dashboard/dashboard/src/app/api/auth/[...nextauth]/route.ts
+++ Dashboard/dashboard/src/app/api/auth/[...nextauth]/route.ts
@@ -103,7 +103,7 @@ const handler = NextAuth({
   },
   session: {
     strategy: "jwt",
-    maxAge: 30 * 24 * 60 * 60, // 30 days
+    maxAge: 24 * 60 * 60, // 30 days
     updateAge: 24 * 60 * 60, // 24 hours - how frequently to update the session
   },
   secret: process.env.NEXTAUTH_SECRET,
